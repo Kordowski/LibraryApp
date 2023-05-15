0e5f43bf29a80d23392ebf22824d472674fd6cae
Kordowski <rafal@kordowski.pl>
2023-05-15 17:16:53 +0200

    Add project files.

---
+++ LibraryApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using LibraryApp.Repositories;
+using LibraryApp.Entities;
+
+var readersRepository = new GenericRepository<Readers>();
+
+readersRepository.Add(new Readers {FirstName = "Rafal" });
+readersRepository.Add(new Readers {FirstName = "Weronika"});
+readersRepository.Add(new Readers { FirstName = "Kuba" });
+readersRepository.Save();
---
+++ LibraryApp/Repositories/ReadersRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryApp.Entities;
+
+namespace LibraryApp.Repositories
+{
+    internal class ReadersRepository
+    {
+        private readonly List<Readers> _readers = new();
+
+        public void Add(Readers readers)
+        {
+            readers.Id = _readers.Count + 1;
+            _readers.Add(readers);
+        }
+
+        public void Save()
+        {
+            foreach (var reader in _readers)
+            {
+                Console.WriteLine(reader);
+            }
+        }
+
+        public Readers GetById(int id)
+        {
+            return _readers.Single(item => item.Id == id);
+        }
+    }
+}
